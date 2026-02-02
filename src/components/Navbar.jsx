58bc5fc042abf7a9a1eba654983f7ca9022486af
Satyamgupta7705 <forsatyam2018@gmail.com>
2026-02-02 13:37:01 +0530

    fix: Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,35 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/azallogo.jpeg";
 
 const Navbar = () => {
   return (
-    <nav className="w-full bg-white shadow flex items-center px-6 py-2 md:py-4">
-      {/* Left Logo */}
-      <div className="flex-1">
-        <img
-          src="/src/assets/azallogo.jpeg"
-          alt="Logo"
-          className="h-12 md:h-20 lg:h-24 w-auto cursor-pointer"
-        />
+    <nav className="w-full bg-white shadow px-4 md:px-6">
+      <div className="flex items-center min-h-16 md:min-h-24 lg:min-h-30">
+        {/* Left Logo */}
+        <div className="shrink-0">
+          <img
+            src={logo}
+            alt="Logo"
+            className="h-10 md:h-16 lg:h-20 w-auto object-contain cursor-pointer"
+          />
+        </div>
+
+        {/* Center Menu */}
+        <ul className="flex gap-4 md:gap-8 mx-auto text-gray-700 font-medium text-sm md:text-base lg:text-lg">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+        </ul>
       </div>
-
-      {/* Center Menu */}
-      <ul className="flex gap-4 md:gap-8 flex-1 justify-center text-gray-700 font-medium text-sm md:text-base lg:text-lg">
-        <li>
-          <Link to="/" className="hover:text-black transition">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/services" className="hover:text-black transition">
-            Services
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" className="hover:text-black transition">
-            Contact
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" className="hover:text-black transition">
-            About Us
-          </Link>
-        </li>
-      </ul>
-
-      {/* Right Spacer */}
-      <div className="flex-1" />
     </nav>
   );
 };
